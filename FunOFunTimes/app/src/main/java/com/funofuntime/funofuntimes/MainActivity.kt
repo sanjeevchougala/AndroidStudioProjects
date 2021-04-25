e9fe697b1c3872d31f750a1c0b8ce2e4279c3027
sanjeevchougala <sanjeevchougala.ckp@gmail.com>
2021-04-25 07:24:39 -0400

    updated LEarn app

--- FunOFunTimes/app/src/main/java/com/funofuntime/funofuntimes/MainActivity.kt
+++ FunOFunTimes/app/src/main/java/com/funofuntime/funofuntimes/MainActivity.kt
@@ -1,18 +1,11 @@
 package com.funofuntime.funofuntimes
 
-import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ImageButton
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var layoutManager: GridLayoutManager
-    lateinit var adapter: ActivitiesSelectorAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -42,23 +35,4 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
-
-//    override fun onResume() {
-//        super.onResume()
-//       // updateRecycler()
-//    }
-
-//    fun updateRecycler() {
-//        var recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-//        var prefs = getSharedPreferences(getString(R.string.SHARED_PREF_NAME), Context.MODE_PRIVATE)
-//        var activities =  prefs.getStringSet(getString(R.string.ACTIVITIES), setOf())?.toMutableSet()
-//
-//        if(activities == null){
-//            activities = mutableSetOf("AddSub", "NumPractice","Tic-Tac-Toe")
-//        }
-//        layoutManager = GridLayoutManager(this,3)
-//        recyclerView.layoutManager = layoutManager
-//        adapter = ActivitiesSelectorAdapter((listOf(findViewById<Button>(R.id.playAddSubButton),findViewById<Button>(R.id.playNumPracticeButton))))
-//        recyclerView.adapter = adapter
-//    }
 }
